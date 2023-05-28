17929daa919c9db07911aaee8ccb30862e99598e
nanda77 <nalandaneeraj.n7@gmail.com>
2023-05-28 19:54:18 +0530

    d3.2

--- lib/receive_page.dart
+++ lib/receive_page.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:lnbits/lnbits.dart';
@@ -11,6 +9,7 @@ class ReceivePage extends StatefulWidget {
   final SharedPreferences prefs;
 
   ReceivePage({required this.api, required this.prefs});
+
   @override
   _ReceivePageState createState() => _ReceivePageState();
 }
@@ -19,47 +18,26 @@ class _ReceivePageState extends State<ReceivePage> {
   final TextEditingController _satsController = TextEditingController();
   final TextEditingController _memoController = TextEditingController();
   bool _isLoading = false;
+  String amount = "0";
 
-  // Future<void> _generateInvoice() async {
-  //   var navigator = Navigator.of(context);
-
-  //   // Generate invoice logic here
-  //   final invoiceData = await widget.api.createInvoice(
-  //       amount: int.parse(_satsController.text), memo: _memoController.text);
-
-  //   // Once the invoice data is received, parse it to a Map<String, dynamic>
-  //   Map<String, dynamic> invoiceMap = jsonDecode(invoiceData);
-
-  //   // Map the parsed data to a PaymentRequest object
-  //   final paymentRequest = PaymentRequest.fromJson(invoiceMap);
-
-  //   // Once the PaymentRequest is generated, navigate to the QrPage:
-  //   navigator.push(
-  //     MaterialPageRoute(
-  //         builder: (context) => QrPage(paymentRequest: paymentRequest)),
-  //   );
-  // }
   Future<void> _generateInvoice() async {
     setState(() {
       _isLoading = true;
     });
     var navigator = Navigator.of(context);
 
-    // Generate invoice logic here
-    final invoiceData = await widget.api.createInvoice(
-        amount: int.parse(_satsController.text), memo: _memoController.text);
+    final invoiceData = await widget.api
+        .createInvoice(amount: int.parse(amount), memo: _memoController.text);
 
-    // If createInvoice only returns the invoice string, pass it directly to QrPage:
     navigator.push(
       MaterialPageRoute(
           builder: (context) => QrPage(
               api: widget.api,
               prefs: widget.prefs,
               invoice: invoiceData,
-              amount: int.parse(_satsController.text),
+              amount: int.parse(amount),
               memo: _memoController.text)),
     );
-    // End loading state
     setState(() {
       _isLoading = false;
     });
@@ -73,172 +51,144 @@ class _ReceivePageState extends State<ReceivePage> {
             size: 50.0,
           )
         : Scaffold(
-            appBar: AppBar(
-              title: Text('Receive'),
-            ),
+            // appBar: AppBar(
+            //   backgroundColor: Theme.of(context).brightness == Brightness.dark
+            //       ? Color(0xFF000000)
+            //       : Color(0xff88a1ac),
+            //   title: Text('Receive'),
+            // ),
             body: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Text(
-                    'Enter amount',
-                    style: TextStyle(fontSize: 24),
+                    '$amount',
+                    style: TextStyle(fontSize: 47, fontWeight: FontWeight.w900),
                   ),
-                  TextField(
-                    controller: _satsController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Enter sats',
+                  Text(
+                    'sats',
+                    style: TextStyle(fontSize: 17, fontWeight: FontWeight.w400),
+                  ),
+                  SizedBox(height: 100),
+                  Container(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    width: double.infinity,
+                    child: TextField(
+                      controller: _memoController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Enter note',
+                        hintStyle: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w600,
+                          color: Color(0xFF88a1ac),
+                        ),
+                      ),
                     ),
                   ),
-                  TextField(
-                    controller: _memoController,
-                    decoration: InputDecoration(
-                      labelText: 'Enter memo',
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        for (var i = 1; i <= 3; i++) _buildButton(i.toString()),
+                      ],
                     ),
                   ),
-                  ElevatedButton(
-                    onPressed: _generateInvoice,
-                    child: Text('Generate Invoice'),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        for (var i = 4; i <= 6; i++) _buildButton(i.toString()),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        for (var i = 7; i <= 9; i++) _buildButton(i.toString()),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8, right: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buildButton("Clr"),
+                        _buildButton("0"),
+                        _buildButton("Del"),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.only(left: 6, right: 6),
+                    width: double
+                        .infinity, // This will make the button span the full width of the screen
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.only(top: 20, bottom: 20),
+                        backgroundColor:
+                            Color(0xFF21728D), // #88a1ac with 100% alpha
+                        side: BorderSide(
+                            color: Color(0x1A88a1ac)), // border color
+                      ),
+                      onPressed: _generateInvoice,
+                      child: Text(
+                        'Generate Invoice',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                    ),
                   ),
                 ],
               ),
             ),
           );
   }
-}
 
-class PaymentRequest {
-  final String paymentRequest;
-  final String paymentHash;
-  final int amount;
-  final String memo;
-
-  PaymentRequest({
-    required this.paymentRequest,
-    required this.paymentHash,
-    required this.amount,
-    required this.memo,
-  });
-
-  factory PaymentRequest.fromJson(Map<String, dynamic> json) {
-    return PaymentRequest(
-      paymentRequest: json['payment_request'],
-      paymentHash: json['payment_hash'],
-      amount: json['amount'],
-      memo: json['memo'],
+  Widget _buildButton(String label) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Color(0x00ffffff),
+        shadowColor: Color(0x0088a1ac), // #88a1ac with 100% alpha
+        side: BorderSide(color: Color(0x1A88a1ac)), // border color
+      ),
+      onPressed: () => _onPressed(label),
+      child: Text(
+        label,
+        style: TextStyle(color: Color(0xFF88a1ac), fontSize: 16),
+      ),
     );
   }
+
+  _onPressed(String label) {
+    setState(() {
+      if (label == "Clr") {
+        amount = "0";
+      } else if (label == "Del") {
+        if (amount.length > 1) {
+          amount = amount.substring(0, amount.length - 1);
+        } else {
+          amount = "0";
+        }
+      } else {
+        if (amount == "0") {
+          amount = label;
+        } else {
+          amount += label;
+        }
+      }
+    });
+  }
 }
-////
-///
-///
-///
-///
-// import 'dart:convert';
-
-// import 'package:flutter/material.dart';
-// import 'package:lnbits/lnbits.dart';
-// import 'package:ninjapay/qr_page.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class ReceivePage extends StatefulWidget {
-//   final LNBitsAPI api;
-//   final SharedPreferences prefs;
-
-//   ReceivePage({required this.api, required this.prefs});
-//   @override
-//   _ReceivePageState createState() => _ReceivePageState();
-// }
-
-// class _ReceivePageState extends State<ReceivePage> {
-//   final TextEditingController _satsController = TextEditingController();
-//   final TextEditingController _memoController = TextEditingController();
-
-//   // Future<void> _generateInvoice() async {
-//   //   var navigator = Navigator.of(context);
-
-//   //   // Generate invoice logic here
-//   //   final invoiceData = await widget.api.createInvoice(
-//   //       amount: int.parse(_satsController.text), memo: _memoController.text);
-
-//   //   // Once the invoice data is received, parse it to a Map<String, dynamic>
-//   //   Map<String, dynamic> invoiceMap = jsonDecode(invoiceData);
-
-//   //   // Map the parsed data to a PaymentRequest object
-//   //   final paymentRequest = PaymentRequest.fromJson(invoiceMap);
-
-//   //   // Once the PaymentRequest is generated, navigate to the QrPage:
-//   //   navigator.push(
-//   //     MaterialPageRoute(
-//   //         builder: (context) => QrPage(paymentRequest: paymentRequest)),
-//   //   );
-//   // }
-
-//   // Future<void> _generateInvoice() async {
-//   //   var navigator = Navigator.of(context);
-
-//   //   // Generate invoice logic here
-//   //   final invoiceData = await widget.api.createInvoice(
-//   //       amount: int.parse(_satsController.text), memo: _memoController.text);
-
-//   //   // If createInvoice only returns the invoice string, pass it directly to QrPage:
-//   //   navigator.push(
-//   //     MaterialPageRoute(builder: (context) => QrPage(invoice: invoiceData)),
-//   //   );
-//   // }
-
-//   Future<void> _generateInvoice() async {
-//     var navigator = Navigator.of(context);
-
-//     // Generate invoice logic here
-//     final invoiceData = await widget.api.createInvoice(
-//         amount: int.parse(_satsController.text), memo: _memoController.text);
-
-//     // Once the Invoice is generated, navigate to the QrPage:
-//     navigator.push(
-//       MaterialPageRoute(
-//           builder: (context) => QrPage(
-//               invoice: invoiceData,
-//               amount: int.parse(_satsController.text),
-//               memo: _memoController.text)),
-//     );
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Receive'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Column(
-//           children: [
-//             Text(
-//               '0 sats',
-//               style: TextStyle(fontSize: 24),
-//             ),
-//             TextField(
-//               controller: _satsController,
-//               keyboardType: TextInputType.number,
-//               decoration: InputDecoration(
-//                 labelText: 'Enter sats',
-//               ),
-//             ),
-//             TextField(
-//               controller: _memoController,
-//               decoration: InputDecoration(
-//                 labelText: 'Enter memo',
-//               ),
-//             ),
-//             ElevatedButton(
-//               onPressed: _generateInvoice,
-//               child: Text('Generate Invoice'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
