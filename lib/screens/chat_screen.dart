9167aac21a10bc6da0e4340e71451edb66bbc472
nanda77 <nalandaneeraj.n7@gmail.com>
2023-11-27 01:33:20 +0530

    AI Assistants functions working

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -26,128 +26,83 @@ class _ChatScreenState extends State<ChatScreen> {
   Future<void> _processMessage(String message) async {
     print("Sending message to GPT: $message");
 
-    // Create a new thread
-    var createThreadUrl = Uri.parse('$apiUrlBase/threads');
-    var threadResponse = await http.post(
-      createThreadUrl,
+    var chatCompletionUrl = Uri.parse('$apiUrlBase/chat/completions');
+    var chatResponse = await http.post(
+      chatCompletionUrl,
       headers: {
         'Authorization': 'Bearer $openaiApiKey',
         'Content-Type': 'application/json',
-        'OpenAI-Beta': 'assistants=v1'
       },
-    );
-
-    print("Thread creation response: ${threadResponse.statusCode}");
-    if (threadResponse.statusCode == 200) {
-      var threadData = jsonDecode(threadResponse.body);
-      var threadId = threadData['id']; // Extract the thread ID
-      print("Thread ID: $threadId");
-
-      // Add a message to the thread
-      var addMessageUrl = Uri.parse('$apiUrlBase/threads/$threadId/messages');
-      var messageResponse = await http.post(
-        addMessageUrl,
-        headers: {
-          'Authorization': 'Bearer $openaiApiKey',
-          'Content-Type': 'application/json',
-          'OpenAI-Beta': 'assistants=v1'
-        },
-        body: jsonEncode({
-          'role': 'user',
-          'content': message,
-        }),
-      );
-
-      print("Message addition response: ${messageResponse.statusCode}");
-
-      // Run the assistant
-      var runUrl = Uri.parse('$apiUrlBase/threads/$threadId/runs');
-      var runResponse = await http.post(
-        runUrl,
-        headers: {
-          'Authorization': 'Bearer $openaiApiKey',
-          'Content-Type': 'application/json',
-          'OpenAI-Beta': 'assistants=v1'
-        },
-        body: jsonEncode({
-          'assistant_id': assistantId,
-        }),
-      );
-
-      // Check for the assistant's response
-      if (runResponse.statusCode == 200) {
-        // Wait for the run to complete and then retrieve the assistant's response
-        var runData = jsonDecode(runResponse.body);
-        var runId = runData['id'];
-
-        bool runCompleted = false;
-        while (!runCompleted) {
-          await Future.delayed(
-              Duration(seconds: 1)); // Delay to prevent rapid polling
-
-          var runStatusUrl =
-              Uri.parse('$apiUrlBase/threads/$threadId/runs/$runId');
-          var runStatusResponse = await http.get(
-            runStatusUrl,
-            headers: {
-              'Authorization': 'Bearer $openaiApiKey',
-              'OpenAI-Beta': 'assistants=v1'
-            },
-          );
-
-          if (runStatusResponse.statusCode == 200) {
-            var runStatusData = jsonDecode(runStatusResponse.body);
-            runCompleted = runStatusData['status'] == 'completed';
-          } else {
-            print("Error fetching run status: ${runStatusResponse.body}");
-            break;
-          }
-        }
-        // Retrieve the assistant's response
-        var messagesUrl = Uri.parse('$apiUrlBase/threads/$threadId/messages');
-        var messagesResponse = await http.get(
-          messagesUrl,
-          headers: {
-            'Authorization': 'Bearer $openaiApiKey',
-            'OpenAI-Beta': 'assistants=v1'
-          },
-        );
-
-        print("Messages retrieval response: ${messagesResponse.statusCode}");
-        if (messagesResponse.statusCode == 200) {
-          var messagesData = jsonDecode(messagesResponse.body);
-          var assistantMessages = messagesData['data']
-              .where((msg) => msg['role'] == 'assistant')
-              .toList();
-          print("Message: $messagesData");
-          print("Assistant Message: $assistantMessages");
-
-          // Assuming the last assistant message contains the action response
-          if (assistantMessages.isNotEmpty) {
-            var lastAssistantMessage = assistantMessages.last;
-            print("Last Assistant Message: $lastAssistantMessage");
-            print("Content: ${lastAssistantMessage['content']}");
-            print("Text: ${lastAssistantMessage['content']['text']}");
-            var assistantResponse =
-                lastAssistantMessage['content']['text']['value'];
-
-            print("Assistant's response: $assistantResponse");
-            setState(() {
-              _messages.insert(0, "Bot: $assistantResponse");
-            });
-
-            // Check if the assistant's response indicates an action to create an invoice
-            if (assistantResponse.contains('action:createInvoice')) {
-              // Parse the response to extract the amount and memo
-              var amount = lastAssistantMessage['parameters']['amount'];
-              var memo = lastAssistantMessage['parameters']['memo'];
-              if (amount != null && memo != null) {
-                await _createInvoice(amount, memo);
-              }
+      body: jsonEncode({
+        'model': 'gpt-3.5-turbo-1106',
+        'messages': [
+          {'role': 'user', 'content': message}
+        ],
+        'tools': [
+          // Add the tools array here
+          {
+            'type': 'function',
+            'function': {
+              'name': 'createInvoice',
+              'description':
+                  'Create an invoice with specified amount and memo, always ask for user for amount input if not given',
+              'parameters': {
+                'type': 'object',
+                'properties': {
+                  'amount': {
+                    'type': 'integer',
+                    'description':
+                        'The amount for the invoice which is required by user as an input always'
+                  },
+                  'memo': {
+                    'type': 'string',
+                    'description': 'An memo or description for the invoice'
+                  }
+                },
+                'required': ['amount', 'memo']
+              },
             }
           }
+        ],
+        'tool_choice': 'auto'
+      }),
+    );
+
+    print("Chat completion response: ${chatResponse.statusCode}");
+    if (chatResponse.statusCode == 200) {
+      var chatData = jsonDecode(chatResponse.body);
+      print("Chat data: $chatData");
+
+      var choices = chatData['choices'];
+      if (choices.isNotEmpty) {
+        var assistantMessage = choices[0]['message'];
+        print("Assistant's response: $assistantMessage");
+
+        // Check if the assistant's response contains a function call
+        if (assistantMessage['tool_calls'] != null &&
+            assistantMessage['tool_calls'].isNotEmpty) {
+          var toolCall = assistantMessage['tool_calls'][0];
+          var functionName = toolCall['function']['name'];
+
+          // Handling the createInvoice function call
+          if (functionName == 'createInvoice') {
+            var functionArgs = jsonDecode(toolCall['function']['arguments']);
+            int amount = functionArgs['amount'];
+            String memo = functionArgs['memo'];
+
+            if (amount != null && memo != null) {
+              await _createInvoice(amount, memo);
+            }
+          }
+        } else if (assistantMessage['content'] != null) {
+          // Handling regular text responses
+          setState(() {
+            _messages.insert(0, "Bot: ${assistantMessage['content']}");
+          });
         }
       }
+    } else {
+      print("Error in chat completion: ${chatResponse.body}");
     }
   }
 
@@ -157,27 +112,33 @@ class _ChatScreenState extends State<ChatScreen> {
       "X-Api-Key": "c6bda6e5c9374c21a5cdee58572f08e1",
       "Content-type": "application/json"
     };
-    var body = jsonEncode({"out": false, "amount": amount, "memo": memo});
+    var body = jsonEncode({
+      "out": false,
+      "amount": amount,
+      "memo": memo
+      // Add other fields if necessary, such as "expiry", "unit", "webhook", "internal"
+    });
 
     try {
       var response = await http.post(url, headers: headers, body: body);
-      if (response.statusCode == 200) {
+      if (response.statusCode == 201) {
         var responseData = jsonDecode(response.body);
-        // Assuming the response contains some sort of success message or invoice ID
-        var invoiceId =
-            responseData['id']; // Adjust this based on actual response
-        print("Invoice created successfully: $invoiceId");
+        var paymentRequest = responseData['payment_request'];
+        print(
+            "Invoice created successfully: Payment Request - $paymentRequest");
         setState(() {
           _messages.insert(
-              0, "Bot: Invoice created successfully. ID: $invoiceId");
+              0, "Bot: Invoice created. Payment Request: $paymentRequest");
         });
       } else {
+        print("Failed to create invoice. Response: ${response.body}");
         setState(() {
           _messages.insert(0,
               "Bot: Failed to create invoice. Status code: ${response.statusCode}");
         });
       }
     } catch (e) {
+      print("Error occurred while creating invoice: $e");
       setState(() {
         _messages.insert(
             0, "Bot: Error occurred while creating invoice. Error: $e");
@@ -207,12 +168,18 @@ class _ChatScreenState extends State<ChatScreen> {
                   child: TextField(
                     controller: _controller,
                     decoration: InputDecoration(hintText: 'Send a message'),
-                    onSubmitted: _sendMessage,
+                    onSubmitted: (text) {
+                      // Clear the text field without sending the message again
+                      _controller.clear();
+                    },
                   ),
                 ),
                 IconButton(
                   icon: Icon(Icons.send),
-                  onPressed: () => _sendMessage(_controller.text),
+                  onPressed: () {
+                    _sendMessage(_controller.text);
+                    _controller.clear(); // Clear text after sending
+                  },
                 ),
               ],
             ),
